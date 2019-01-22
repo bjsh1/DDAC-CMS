ab9d271c4f532ae9a9047ad2a4c4832e1cd0e2f7
Bijay Shrestha <41225068+bjsh1@users.noreply.github.com>
2019-01-22 14:18:29 +0545

    Update config.php

--- admin/inc/config.php
+++ admin/inc/config.php
@@ -3,9 +3,9 @@
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_SERVER', 'mysqlbj.mysql.database.azure.com');
+define('DB_USERNAME', 'bijay@mysqlbj');
+define('DB_PASSWORD', '9818756516Bj');
 define('DB_NAME', 'cloud');
  
 /* Attempt to connect to MySQL database */
