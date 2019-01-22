601dec453f8d5958e597393fdbfb592ebab80ad1
Bijay Shrestha <41225068+bjsh1@users.noreply.github.com>
2019-01-22 14:04:10 +0545

    Update config.php

--- inc/config.php
+++ inc/config.php
@@ -1,14 +1,18 @@
 
 <?php
 /* Database credentials. Assuming you are running MySQL
-server with default setting (user 'root' with no password) */
+server with default setting (user 'root' with no password) 
 
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 define('DB_NAME', 'cloud');
  
-/* Attempt to connect to MySQL database */
+ Attempt to connect to MySQL database */
+
+$con=mysqli_init(); 
+mysqli_ssl_set($con, NULL, NULL, {ca-cert filename}, NULL, NULL); 
+mysqli_real_connect($con, "mysqlbj.mysql.database.azure.com", "bijay@mysqlbj", {9818756516Bj}, {cloud}, 3306);
 
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
  
